ee242c760eb85248f3ac1fe6498a94e6cf96818b
hajaratali <hazarathalibibijan63@gmail.com>
2026-05-12 22:04:59 +0530

    Day-1 java-Dsa-5/12/2026-pattern-printing

--- src/pattern_printing/stat_rectangle.java
+++ src/pattern_printing/stat_rectangle.java
@@ -6,11 +6,15 @@ public class stat_rectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows : ");
+        System.out.println("Enter the numbrt of columns");
         int n = sc.nextInt();
+        int m = sc.nextInt();
         System.out.println("The star Rectangle ");
         System.out.println("-----------------");
-        for(int i =0;i<=n;i++){
-            for (int j = 0; j < n; j++) { 
+        for(int i =1;i<=n;i++){
+            System.out.print(i+" ");
+            for (int j = 1; j <= m; j++) { 
+                System.out.print(j+" ");
                 System.out.print("*"+" ");      
             }
         System.out.println(); 
